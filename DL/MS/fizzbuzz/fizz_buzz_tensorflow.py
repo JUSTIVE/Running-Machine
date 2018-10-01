75bc289b764bc2203c3de7a1b76241dbe0530117
Ben <ben399399@naver.com>
2018-10-01 21:03:15 +0900

    Hard time on optimizing

--- DL/MS/fizzbuzz/fizz_buzz_tensorflow.py
+++ DL/MS/fizzbuzz/fizz_buzz_tensorflow.py
@@ -33,27 +33,24 @@ def initWeights(shape):
 
 # 본 뉴럴 네트워크의 모델
 def model(X,
-    w_h,b_h,
-    w_o,b_o):
-    h = tf.nn.relu(tf.matmul(X,w_h)+b_h)# 렐루 활성화 함수 (1*3) * (3*3)= (1*3)
-    return tf.matmul(h,w_o)
-
+    w_h,w_h2,
+    w_o,):
+    h = tf.nn.relu(tf.matmul(X,w_h))# 렐루 활성화 함수 (1*3) * (3*3)= (1*3)
+    h2 = tf.nn.relu(tf.matmul(h,w_h2))
+    return tf.matmul(h2,w_o)
 
 # 입력 레이어 10차원을 설정
 X = tf.placeholder("float",[None, NUM_DIGITS])
 Y = tf.placeholder("float",[None,4])
 
-actual = ['1','2','fizz','4','buzz','fizz','7','8','fizz','buzz','11','fizz','13','14','fizzbuzz','16','17','fizz','19','buzz','fizz','22','23','fizz','buzz','26','fizz','28','29','fizzbuzz','31','32','fizz','34','buzz','fizz','37','38','fizz','buzz','41','fizz','43','44','fizzbuzz','46','47','fizz','49','buzz','fizz','52','53','fizz','buzz','56','fizz','58','59','fizzbuzz','61','62','fizz','64','buzz','fizz','67','68','fizz','buzz','71','fizz','73','74','fizzbuzz','76','77','fizz','79','buzz','fizz','82','83','fizz','buzz','86','fizz','88','89','fizzbuzz','91','92','fizz','94','buzz','fizz','97','98','fizz','buzz']
-
-NUM_HIDDEN = 250
+NUM_HIDDEN = 500
 w_h = initWeights([NUM_DIGITS,NUM_HIDDEN])#히든 레이어 설정 
-b_h = initWeights([NUM_HIDDEN])# 히든레이어 바이어스
+w_h2 = initWeights([NUM_HIDDEN,NUM_HIDDEN])#히든 레이어 설정 
 w_o = initWeights([NUM_HIDDEN, 4])# 출력 레이어 설정
-b_o = initWeights([4])# 히든-히든레이어 설정
 
 py_x = model(X,
-    w_h,b_h,
-    w_o,b_o)# 모델 인스턴스 생성
+    w_h,w_h2,
+    w_o)# 모델 인스턴스 생성
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y,logits=py_x))
 train_op = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
@@ -108,5 +105,6 @@ with tf.Session() as sess:
 
     print(output)
 
+actual = ['1','2','fizz','4','buzz','fizz','7','8','fizz','buzz','11','fizz','13','14','fizzbuzz','16','17','fizz','19','buzz','fizz','22','23','fizz','buzz','26','fizz','28','29','fizzbuzz','31','32','fizz','34','buzz','fizz','37','38','fizz','buzz','41','fizz','43','44','fizzbuzz','46','47','fizz','49','buzz','fizz','52','53','fizz','buzz','56','fizz','58','59','fizzbuzz','61','62','fizz','64','buzz','fizz','67','68','fizz','buzz','71','fizz','73','74','fizzbuzz','76','77','fizz','79','buzz','fizz','82','83','fizz','buzz','86','fizz','88','89','fizzbuzz','91','92','fizz','94','buzz','fizz','97','98','fizz','buzz']
 correct = [(x == y) for x, y in zip(actual, output)]
 print('correct',sum(correct))
