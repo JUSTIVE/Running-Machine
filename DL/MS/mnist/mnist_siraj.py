e39ef62c1e084b3938c91fdd59a7706e9221dd84
Ben <ben399399@naver.com>
2018-10-27 17:30:30 +0900

    Fixed partial greek to simoid

--- DL/MS/mnist/mnist_siraj.py
+++ DL/MS/mnist/mnist_siraj.py
@@ -62,11 +62,11 @@ with tf.Session() as sess:
     writer = tf.summary.FileWriter("./siraj/", sess.graph) # for 0.8
     merged = tf.summary.merge_all()
 
-    # Step 11 you need to initialize all variables
+    # Step 11 you need to initialize all variables  
     tf.global_variables_initializer().run()
 
     #Step 12 train the  model
-    for i in range(100):
+    for i in range(3):
         for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
             sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
                                           p_keep_input: 0.8, p_keep_hidden: 0.5})
